9f2377fc6487248b4fd93e9d540fc1a2a14807ed
Jérémy Buget <jbuget@octo.com>
2017-01-07 02:27:02 +0100

    Initialize repository

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <leaflet-map></leaflet-map>
+  </div>
+</template>
+
+<script>
+import LeafletMap from './components/LeafletMap';
+
+export default {
+  name: 'app',
+  components: {
+    LeafletMap,
+  },
+};
+</script>
+
+<style>
+
+  body { margin:0; padding:0; }
+
+</style>
---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="leaflet-map">
+    <div id="map" class="map"></div>
+  </div>
+</template>
+
+<script>
+  import L from 'leaflet';
+
+  export default {
+    name: 'leaflet-map',
+    mounted: () => {
+      const map = L.map('map', {
+        center: [48.868, 2.333],
+        zoom: 13,
+      });
+      L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: 'pk.eyJ1IjoiamJ1Z2V0IiwiYSI6ImNpeG1pdXIxdjAxdXEyeHBoYjA4c25rengifQ.h8XHihbWbP1rGv7GzZypdA',
+      }).addTo(map);
+    },
+  };
+</script>
+
+<style scoped>
+
+  @import '../../node_modules/leaflet/dist/leaflet.css';
+
+  #map {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+  }
+
+</style>
