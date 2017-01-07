e18b3b28fc708ca02c57ab3d7b67ffc87523e5ea
Jérémy Buget <jbuget@octo.com>
2017-01-07 11:30:00 +0100

    Refactor LeafletMap by extracting data + remove useless Hello default component

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -9,11 +9,14 @@
 
   export default {
     name: 'leaflet-map',
-    mounted: () => {
-      const map = L.map('map', {
+    data() {
+      return {
         center: [48.868, 2.333],
         zoom: 13,
-      });
+      };
+    },
+    mounted() {
+      const map = L.map('map', this.$data);
       L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
